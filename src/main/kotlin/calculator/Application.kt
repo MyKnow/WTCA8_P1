003196c92ac22a68adb0572e81998d6a84f5f30c
MyKnow <myknow000@gmail.com>
2025-10-16 20:09:15 +0900

    feat(main): implement main function structure with TODOs
    - Set up main function with placeholders for input handling, delimiter extraction, number parsing, and result calculation.
    - Prepare for further implementation of calculator logic.

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,19 @@
 package calculator
 
 fun main() {
-    // TODO: 프로그램 구현
+    try {
+        // 1. Console로부터 문자열 입력 받음
+        val input: String = TODO()
+
+        // 2. 입력된 문자열에서 커스텀 구분자 찾음
+        val customDelimiters: Array<Char> = TODO()
+
+        // 3. 기본 구분자 + 커스텀 구분자로 숫자 구분함
+        val parsedNumbers: Array<Int> = TODO()
+
+        // 4. 구분된 숫자들을 모두 더하여 출력함
+        val result: String = TODO()
+    } catch (e: IllegalArgumentException) {
+        println(e)
+    }
 }
