593d28427ea912cf00163f8f565b3c763955df78
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-10-20 23:57:40 +0900

    docs(InputView): Enhance InputView with method documentation
    Add documentation for readNonEmptyLine function

--- src/main/kotlin/calculator/io/InputView.kt
+++ src/main/kotlin/calculator/io/InputView.kt
@@ -3,6 +3,13 @@ package calculator.io
 import camp.nextstep.edu.missionutils.Console
 
 object InputView {
+
+    /**
+     * 사용자로부터 빈 문자열이 아닌 입력을 한 줄 받아 반환함.
+     *
+     * @return 공백이 제거된, 빈 문자열이 아닌 사용자 입력 문자열
+     * @throws IllegalArgumentException 입력이 빈 문자열인 경우 예외가 발생.
+     */
     fun readNonEmptyLine(): String {
         println("덧셈할 문자열을 입력해 주세요.")
         val input: String = Console.readLine().trim()
